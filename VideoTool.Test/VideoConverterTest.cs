c1575db478c47200bd20238c288daaabd7d2c59b
Thomas Holmes <thomasholmes@outlook.com>
2020-12-22 23:27:44 +0000

    Added Linux specific version check.

--- VideoTool.Test/VideoConverterTest.cs
+++ VideoTool.Test/VideoConverterTest.cs
@@ -3,6 +3,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.IO;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.InteropServices;
 
 namespace VideoTool.Test
 {
@@ -61,7 +62,15 @@ namespace VideoTool.Test
         public async Task FfmpegTest()
         {
             string version = await videoConverter.FetchFfmpegVersion();
-            Assert.AreEqual("2020-12-15-git-32586a42da-essentials_build-www.gyan.dev", version);
+
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                Assert.AreEqual("2020-12-15-git-32586a42da-essentials_build-www.gyan.dev", version);
+            }
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                Assert.AreEqual("4.3.1-static", version);
+            }
         }
 
         [TestMethod]
